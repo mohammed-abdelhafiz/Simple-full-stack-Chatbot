b17d8e354db6dde2aadbac4755681693eed125cb
Mohamed Abdelhafiz <mohamed.abdelhafiz.dev@gmail.com>
2026-08-15 18:15:34 +0300

    connect the frontend and backend

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -8,6 +8,10 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.get("/api/hello", (req, res) => {
+  res.json({ message: "Hello from server" });
+});
+
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
